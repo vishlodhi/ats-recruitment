fe09ab1818bd3fc7e785ec8b158c3335e9ada534
Vishal Lodhi <64070785+vishlodhi@users.noreply.github.com>
2026-06-21 12:20:26 +0530

    Add the login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,29 @@
 
+import { useForm } from 'react-hook-form'
+
+
 const Login = () => {
-  return (
-  <h1 className="text-3xl font-bold text-center mt-10">Login Page</h1>
-  )
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='h-screen flex justify-center items-center'>
+                <div className='flex flex-col gap-4'>
+                    <div className='flex items-center gap-4'>
+                        <p>Enter Email</p><input type="email" placeholder='email' {...register('email', { required: true })} />
+                        {errors.email && <p>Email is required</p>}
+                    </div>
+                    <div className='flex items-center gap-4'>
+                        <p>Enter Password</p> <input type="password" {...register('password', { required: true })} placeholder='password' />
+                         {errors.password && <p>Password is required</p>}
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default Login
